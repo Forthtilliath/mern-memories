7f4cec85b5d12fd9953c8ebc0b1fab00331a8b30
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-24 18:54:41 +0200

    recommanded posts

--- client/src/components/PostDetails/PostDetails.js
+++ client/src/components/PostDetails/PostDetails.js
@@ -4,9 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useHistory } from 'react-router-dom';
 
-
 import useStyles from './styles';
-import { getPost } from '../../actions/posts';
+import { getPost, getPostsBySearch } from '../../actions/posts';
 
 const PostDetails = () => {
     const { post, posts, isLoading } = useSelector((state) => state.posts);
@@ -20,6 +19,10 @@ const PostDetails = () => {
         dispatch(getPost(id));
     }, [id]);
 
+    useEffect(() => {
+        dispatch(getPostsBySearch({ search: 'none', tags: post?.tags.join(',') }));
+    }, [post]);
+
     if (!post) return null;
 
     if (isLoading) {
@@ -27,8 +30,12 @@ const PostDetails = () => {
             <Paper elevation={6} className={classes.loadingPaper}>
                 <CircularProgress size="7em" />
             </Paper>
-        )
+        );
     }
+
+    const recommandedPosts = posts.filter(({ _id }) => _id !== post._id);
+
+    const openPost = (id) => history.push(`/posts/${id}`);
 
     return (
         <Paper style={{ padding: '20px', borderRadius: '15px' }} elevation={6}>
@@ -66,6 +73,25 @@ const PostDetails = () => {
                     />
                 </div>
             </div>
+            {!!recommandedPosts.length && (
+                <div className={classes.section}>
+                    <Typography gutterBottom variant="h5">
+                        You might alse like:
+                    </Typography>
+                    <Divider />
+                    <div className={classes.recommendedPosts}>
+                        {recommandedPosts.map(({ title, message, name, likes, selectedFile, _id }) => (
+                            <div key={_id} style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)}>
+                                <Typography gutterBottom variant="h6">{title}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{name}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{message}</Typography>
+                                <Typography gutterBottom variant="subtitle1">Likes: {likes.length}</Typography>
+                                <img src={selectedFile} width="200px" alt={title} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </Paper>
     );
 };
